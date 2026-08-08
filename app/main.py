de40f1d2f7a368ff05578276b25ab84ec4c6f94b
RAZ11 <maurya.mail11@gmail.com>
2026-08-08 15:29:58 +0530

    fast api added for generate news

--- app/main.py
+++ app/main.py
@@ -1,2 +1,21 @@
-#from typing import TypedDict
+from fastapi import FastAPI
+from app.graph.workflow import graph
+from app.schemas.news import NewsRequest
 
+app = FastAPI(
+    title="AI News Writer"
+)
+
+@app.post("/generate_news")
+def generate_news(request:NewsRequest):
+    result = graph.invoke(
+        {
+            "topic": request.topic
+        }
+    )
+
+    return {
+        "topic": result["topic"],
+        "article": result["final_article"],
+        "fact_check": result["fact_check_report"]
+    }
