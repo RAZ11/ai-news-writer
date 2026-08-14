a60c23f16cb2d7574f29ec8d9028e43a08fab0db
RAZ11 <maurya.mail11@gmail.com>
2026-08-14 13:19:52 +0530

    bedrock services issue fix

--- app/services/bedrock_services.py
+++ app/services/bedrock_services.py
@@ -2,7 +2,7 @@ import os
 from langchain_aws import ChatBedrockConverse
 
 bedrock_llm = ChatBedrockConverse(
-    model=os.getenv("BEDROCK_MODEL_NAME"),
+    model=os.getenv("BEDROCK_MODEL_ID"),
     region_name="us-east-1"
 )
 
