7a580d92ee213337123abf79a9905ed994a3d45d
RAZ11 <maurya.mail11@gmail.com>
2026-08-14 14:45:45 +0530

    manage response data

--- app/services/bedrock_services.py
+++ app/services/bedrock_services.py
@@ -6,6 +6,17 @@ bedrock_llm = ChatBedrockConverse(
     region_name="us-east-1"
 )
 
+
 def generate_article(prompt: str):
     response = bedrock_llm.invoke(prompt)
-    return response.content
+
+    if isinstance(response.content, list):
+        text_parts = []
+
+        for item in response.content:
+            if item.get("type") == "text":
+                text_parts.append(item.get("text", ""))
+
+        return "\n".join(text_parts)
+
+    return str(response.content)
